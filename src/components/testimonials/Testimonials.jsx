0a7f44c3e3f55e4d498eb5f3ab6ec1da226fabe4
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-10-18 17:12:45 +0200

    complete implementation of testimonial section &css

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./Testimonials.css"
-
+import LeftArrow from "../../assets/leftArrow.png"
+import RightArrow from "../../assets/rightArrow.png"
 import { testimonialsData } from "../../data/testimonialsData"
 function Testimonials() {
     const [selected, setSelected] = useState(0);
-    const TestiLength = testimonialsData.length
+    const testimonialsLenth = testimonialsData.length
 
     return (
         <div className="Testimonials">
@@ -16,13 +17,31 @@ function Testimonials() {
                     {testimonialsData[selected].review}
                 </span>
                 <span>
-                    <span>
+                    <span style={{ color: "var(--orange)" }}>
                         {testimonialsData[selected].name}
                     </span>{" "}
                     {testimonialsData[selected].status}
                 </span>
             </div>
-            <div className="right-t"></div>
+            <div className="right-t">
+                <div></div>
+                <div></div>
+                <img src={testimonialsData[selected].image} alt="testimonialImage" />
+                <div className="arrows">
+                    <img
+                        onClick={() => {
+                            selected === 0 ? setSelected(testimonialsLenth - 1) :
+                                setSelected((prev) => prev - 1)
+                        }}
+                        src={LeftArrow} alt="leftArrow" />
+                    <img
+                        onClick={() => {
+                            selected === testimonialsLenth - 1 ? setSelected(0) :
+                                setSelected((prev) => prev + 1)
+                        }}
+                        src={RightArrow} alt="rightArrow" />
+                </div>
+            </div>
         </div>
     )
 }
